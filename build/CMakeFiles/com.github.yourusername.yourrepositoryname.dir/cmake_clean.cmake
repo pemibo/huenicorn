5236d164a259a8fa8ef0c4ac2a9e7bda2f4b2ac3
Peter Bogner <pemibo1986@gmail.com>
2017-05-22 15:37:28 +0200

    added .desktop file

---
+++ build/CMakeFiles/com.github.yourusername.yourrepositoryname.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "src/hello-again.c"
+  "com.github.yourusername.yourrepositoryname_valac.stamp"
+  "CMakeFiles/com.github.yourusername.yourrepositoryname.dir/src/hello-again.c.o"
+  "com.github.yourusername.yourrepositoryname.pdb"
+  "com.github.yourusername.yourrepositoryname"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/com.github.yourusername.yourrepositoryname.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/com.github.yourusername.yourrepositoryname.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for com.github.yourusername.yourrepositoryname.
+# This may be replaced when dependencies are built.
